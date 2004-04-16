46cea0e97a9b2bbea8c673874491fd62f725f75c
Sylvain Filteau <cidsphere@gmail.com>
2004-04-16 02:45:58 +0000

    Correction du repository, ignorer

---
+++ Vibrato.cpp
@@ -0,0 +1,32 @@
+#include "vibrato.h"
+#include "LFO.h"
+#include "Tools.h"
+
+CVibrato::CVibrato(CWAVE *son) : CEffet(son)
+{
+}
+
+void CVibrato::Vibrer(float frequence, float mix)
+{
+	CTools tool;
+	CLFO *lfo = new CLFO(LFOSIN, tool.NbEchantillonsParCycle(frequence, m_son->Entete().SampleRate));
+	short *shtCanalG = m_son->getCanalGauche();
+	short *shtCanalD = m_son->getCanalDroite();
+	float tmpLfo;
+
+	for (int i = 0; i < m_son->getNbEchantillon(); i++)
+	{
+		tmpLfo = lfo->getNextValeur();
+		if (tmpLfo != 1)
+		{
+			shtCanalG[i] = (short)(((1-mix)*shtCanalG[i]) + ((shtCanalG[i] * tmpLfo) * mix));
+			
+			if (m_son->Entete().NumChannels == 2)
+			{
+				shtCanalD[i] = (short)(((1-mix)*shtCanalD[i]) + ((shtCanalD[i] * tmpLfo) * mix));
+			}
+		}
+	}
+
+	delete lfo;
+}
